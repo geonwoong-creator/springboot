a7cf6fddb3a81865313decbc8f54d0c563390e08
geonwoong <rjsdnd0420@naver.com>
2022-05-24 17:12:16 +0900

    sprimg boot

---
+++ src/main/java/kopo/poly/dto/ChatDTO.java
@@ -0,0 +1,14 @@
+package kopo.poly.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class ChatDTO {
+
+    private String roomKey = "";
+    private String userNm = "";
+    private String msg = "";
+    private String dateTime = "";
+}
